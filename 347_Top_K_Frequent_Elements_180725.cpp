8a450db4ac6b0f200600490bea8367fc2e94b78a
Richard <mrcjava@gmail.com>
2018-07-26 00:20:23 +0800

    Update 347_Top_K_Frequent_Elements_180725.cpp

--- 347_Top_K_Frequent_Elements_180725.cpp
+++ 347_Top_K_Frequent_Elements_180725.cpp
@@ -1,26 +1,25 @@
 class Solution {
-public
-    vectorint topKFrequent(vectorint& nums, int k) {
-        vectorint res(k,0);
-        mapint,int table;
-        for(auto inums){
-            table[i]++;
-        }
-        
-        mapint,intiterator it;
-        int range=table.size();
-        int maxCount=0, maxIndex=0;
-        for(int i=0;ires.size();i++){
-            for(it=table.begin();it!=table.end();it++){
-                if(it-secondmaxCount){
-                    maxCount=it-second;
-                    maxIndex=it-first;
-                }
-            }
-            res[i]=maxIndex;
-            table.erase(maxIndex);
-        cout.;
-        }
-    return res;
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+	    vector<int> res;
+	    map<int, int> table;
+        vector<int>::iterator iter;
+	    for (iter=nums.begin();iter!=nums.end();iter++) {
+		    table[*iter]++;
+	    }
+
+	    for (int i = 0; i < k; i++) {
+		    int maxCount = 0, maxIndex = 0;
+		    map<int, int>::iterator it;
+		    for (it = table.begin(); it != table.end(); it++) {
+			    if (it->second > maxCount) {
+				    maxCount = it->second;
+				    maxIndex = it->first;
+			    }
+	    	}
+		    res.push_back(maxIndex);
+		    table.erase(maxIndex);
+	    }
+	    return res;
     }
 };
